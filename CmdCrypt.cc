5c40ab626b23038b9b5cef7e13831b787d5fceeb
Dylan Tinianov <dylantinianov@gmail.com>
2017-01-24 22:45:10 -0500

    Take in commands

--- CmdCrypt.cc
+++ CmdCrypt.cc
@@ -13,7 +13,7 @@ void help_print () {
 		"Second argument: FileName -> enter the filename to be encrypted or decrypted ",
 		"\nOPTIONAL ARGUMENTS:",
 		"-key YOUR_KEY -> enter your known decryption key if in decryption mode; else be prompted for it",
-		"-new NEW_FILE_NAME -> encrypt the contents into a new file NEW_FILE_NAME, and only read the second argument",
+		"-new NEW_FILE_NAME -> encrypt/ decrypt the contents into a new file NEW_FILE_NAME; don't write to the second argument",
 		""
 	};
 	for (int i=0; i < help_cmds.size(); i++) {
@@ -22,20 +22,19 @@ void help_print () {
 }
 
 
-// First enter the cmd "encrypt" or "decrypt", then "FileName"
 int main(int argc, char const *argv[]) {
 	if (strcmp(argv[1], "help") == 0) {
 		help_print();
 		return EXIT_SUCCESS;
 	}
-
 	if (argc < 3) {
-		std::cerr << "Error, not all command arguments were entered." << std::endl;
+		std::cerr << "Error, not all necessary command arguments were entered." << std::endl;
 		return EXIT_SUCCESS;
 	}
 
 	const std::string cmd = argv[1];
 	const std::string FILE_NAME = argv[2];
+	std::string OUTPUT_FILE = FILE_NAME; // defult
 
 	std::ifstream user_file;
 	user_file.open(FILE_NAME);
@@ -45,20 +44,50 @@ int main(int argc, char const *argv[]) {
 	}
 
 
+	// Get optional commands
+	std::string decr_key = "";
+	for (int i=2; i < argc; i++) {
+		if (strcmp(argv[i], "-key") == 0) {
+			if (i + 1 < argc) {
+				decr_key = argv[i+1];
+				i++;
+			}
+			else {
+				std::cerr << "Error, missing argument for YOUR_KEY." << std::endl;
+				return EXIT_SUCCESS;
+			}
+
+		}
+
+
+		else if (strcmp(argv[i], "-new") == 0) {
+			if (i + 1 < argc) {
+				OUTPUT_FILE = argv[i+1];
+				i++;
+			}
+			else {
+				std::cerr << "Error, missing argument for NEW_FILE_NAME." << std::endl;
+				return EXIT_SUCCESS;
+			}
+		}
+	}
+
+
+
 	if (cmd == "encrypt") {
+		// TODO: Implement encryption
+		std::string line;
 
 	}
 
+
 	else if (cmd == "decrypt") {
-		std::string decr_key;
-		if (argc == 4) {
-			decr_key = argv[3];
-		}
-		else {
+		if (decr_key == "") {
 			std::cout << "Enter key for decryption: " << std::endl;
 			std::cin >> decr_key;
 		}
 
+		// TODO: Implement decryption
 	}
 
 
---
+++ compile.sh
@@ -0,0 +1,3 @@
+echo Compiling CmdCrypt...
+g++ -std=c++11 CmdCrypt.cc -o cmdcrypt
+echo Completed Successfully
